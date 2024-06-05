fd9901d3badd98cffd3c555f7a1053d152bc6194
deroffal <32491733+deroffal@users.noreply.github.com>
2024-06-05 12:32:23 +0200

    feat: add price calculation - add 2nd service

--- applications/price-api/src/main/java/fr/deroffal/k8slab/priceapi/api/PriceHandler.java
+++ applications/price-api/src/main/java/fr/deroffal/k8slab/priceapi/api/PriceHandler.java
@@ -1,33 +1,34 @@
 package fr.deroffal.k8slab.priceapi.api;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
-import fr.deroffal.k8slab.priceapi.domain.PriceCalculator;
-import java.util.UUID;
+import fr.deroffal.k8slab.priceapi.domain.PriceService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 @Component
 class PriceHandler {
 
-  private final PriceCalculator priceCalculator;
-  private final ApiMapper apiMapper;
+    private final PriceService priceService;
+    private final ApiMapper apiMapper;
 
-  public PriceHandler(PriceCalculator priceCalculator, ApiMapper apiMapper) {
-    this.priceCalculator = priceCalculator;
-    this.apiMapper = apiMapper;
-  }
+    public PriceHandler(PriceService priceService, ApiMapper apiMapper) {
+        this.priceService = priceService;
+        this.apiMapper = apiMapper;
+    }
 
-  public Mono<ServerResponse> getPrice(final ServerRequest request) {
-    return Mono.just(UUID.fromString(request.pathVariable("product")))
-        .flatMap(priceCalculator::getItemPrice)
-        .flatMap(price -> ServerResponse.ok()
-            .contentType(APPLICATION_JSON)
-            .body(BodyInserters.fromValue(apiMapper.toPriceResponse(price)))
-        )
-        .switchIfEmpty(ServerResponse.notFound().build());
-  }
+    public Mono<ServerResponse> getPrice(final ServerRequest request) {
+        return Mono.just(UUID.fromString(request.pathVariable("product")))
+                .flatMap(priceService::getItemPrice)
+                .flatMap(price -> ServerResponse.ok()
+                        .contentType(APPLICATION_JSON)
+                        .body(BodyInserters.fromValue(apiMapper.toPriceResponse(price)))
+                )
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
 }
--- applications/price-api/src/test/java/fr/deroffal/k8slab/priceapi/api/WebFluxTestContextConfiguration.java
+++ applications/price-api/src/test/java/fr/deroffal/k8slab/priceapi/api/WebFluxTestContextConfiguration.java
@@ -1,6 +1,7 @@
 package fr.deroffal.k8slab.priceapi.api;
 
 import fr.deroffal.k8slab.priceapi.domain.PriceCalculator;
+import fr.deroffal.k8slab.priceapi.domain.PriceService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,4 +12,7 @@ public class WebFluxTestContextConfiguration {
 
     @MockBean
     private PriceCalculator priceCalculator;
+
+    @MockBean
+    private PriceService priceService;
 }
