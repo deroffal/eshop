11de74c51049e9ed649dc272b964569d1de95cea
deroffal <32491733+deroffal@users.noreply.github.com>
2024-09-21 11:56:13 +0200

    with reactive web context

--- applications/eshop-price/src/test/java/fr/deroffal/eshop/price/api/ContractsTestClass.java
+++ applications/eshop-price/src/test/java/fr/deroffal/eshop/price/api/ContractsTestClass.java
@@ -1,33 +1,44 @@
 package fr.deroffal.eshop.price.api;
 
 import fr.deroffal.eshop.price.domain.PriceService;
+import fr.deroffal.eshop.price.domain.exception.NotFoundException;
 import fr.deroffal.eshop.price.domain.model.Price;
 import io.restassured.module.webtestclient.RestAssuredWebTestClient;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.web.reactive.context.ReactiveWebApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
-public class ContractsTestClass {
+@WebFluxTest
+@ContextConfiguration(classes = WebFluxTestContextConfiguration.class)
+public abstract class ContractsTestClass {
+
+    @Autowired
+    private PriceService priceService;
+
+    @Autowired
+    private ReactiveWebApplicationContext applicationContext;
 
     @BeforeEach
     public void setUp() {
-        PriceService priceService = Mockito.mock(PriceService.class);
+//        PriceService priceService = Mockito.mock(PriceService.class);
         when(priceService.getItemPrice(UUID.fromString("73aa5936-4410-47a7-96c3-80407b57d710")))
                 .thenReturn(Mono.just(Price.euros(1200)));
 
-        RestAssuredWebTestClient.standaloneSetup(
-                new PriceEndpoint(priceService, new ApiMapperImpl())
-        );
+        doThrow(new NotFoundException("boum !")).when(priceService).getItemPrice(UUID.fromString("e0ddf766-567d-4fbf-9f31-18f6085bc233"));
+
+//        RestAssuredWebTestClient.standaloneSetup(
+//                new PriceEndpoint(priceService, new ApiMapperImpl())
+//        );
+
+        RestAssuredWebTestClient.applicationContextSetup(applicationContext);
 
     }
 
