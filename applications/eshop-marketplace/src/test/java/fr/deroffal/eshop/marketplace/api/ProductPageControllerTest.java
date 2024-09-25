da9cf5bbac97681ae6bfa10419745ddc6d40452a
deroffal <32491733+deroffal@users.noreply.github.com>
2024-09-25 21:18:34 +0200

    add tests

--- applications/eshop-marketplace/src/test/java/fr/deroffal/eshop/marketplace/api/ProductPageControllerTest.java
+++ applications/eshop-marketplace/src/test/java/fr/deroffal/eshop/marketplace/api/ProductPageControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ContextConfiguration(classes = WebFluxApiConfiguration.class)
+@ContextConfiguration(classes = WebTestConfiguration.class)
 @WebMvcTest
 class ProductPageControllerTest {
 
