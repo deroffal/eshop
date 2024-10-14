10f59fee6222ad6003c4a2fae7ba352a77f273d0
deroffal <32491733+deroffal@users.noreply.github.com>
2024-10-14 22:11:00 +0200

    on a quelque chose

--- applications/eshop-marketplace/src/main/java/fr/deroffal/eshop/marketplace/api/CartController.java
+++ applications/eshop-marketplace/src/main/java/fr/deroffal/eshop/marketplace/api/CartController.java
@@ -2,11 +2,17 @@ package fr.deroffal.eshop.marketplace.api;
 
 import fr.deroffal.eshop.marketplace.domain.model.Cart;
 import fr.deroffal.eshop.marketplace.domain.service.CartService;
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +28,8 @@ import static org.springframework.http.HttpStatus.NO_CONTENT;
 @RequestMapping("/cart")
 public class CartController {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CartController.class);
+
     private final CartService service;
     private final CartMapper mapper;
     private final Tracer tracer;
@@ -29,22 +37,56 @@ public class CartController {
     public CartController(CartService service, CartMapper mapper, OpenTelemetry openTelemetry) {
         this.service = service;
         this.mapper = mapper;
-        this.tracer = openTelemetry.getTracer("fr.deroffal.eshop.marketplace", "0.10-span");
+        this.tracer = GlobalOpenTelemetry.getTracer(CartController.class.getName());
+
+//                openTelemetry.getTracer(
+////                "fr.deroffal.eshop.marketplace",
+//                CartController.class.getName(),
+//                "0.10-span"
+//        );
     }
 
     @PostMapping("/")
     public ResponseEntity<CartModel> newCart() {
-        Cart cart = service.create();
 
-        Span span = tracer.spanBuilder("cart").startSpan();
-        span.addEvent("cart created", Attributes.of(
-                AttributeKey.stringKey("date"), DateTimeFormatter.ISO_INSTANT.format(Instant.now()),
-                AttributeKey.stringKey("id"),  cart.id().toString()
-        ));
-        span.setAttribute("cart", cart.id().toString());
 
-        return ResponseEntity.created(URI.create("/cart/" + cart.id()))
-                .body(mapper.toModel(cart));
+        Context current = Context.current();
+        Span span = tracer.spanBuilder("cart")
+                .setParent(current)
+                .startSpan();
+
+
+
+
+
+        SpanContext spanContext = span.getSpanContext();
+        String spanId = spanContext.getSpanId();
+        String traceId = spanContext.getTraceId();
+
+        LOGGER.info("traceId: {}, spanId: {}", traceId, spanId);
+
+        try (Scope scope = span.makeCurrent()) {
+            Cart cart = service.create();
+
+            span.addEvent("cart created", Attributes.of(
+                    AttributeKey.stringKey("date"), DateTimeFormatter.ISO_INSTANT.format(Instant.now()),
+                    AttributeKey.stringKey("id"),  cart.id().toString()
+            ));
+            span.setAttribute("cart", cart.id().toString());
+
+
+            return ResponseEntity.created(URI.create("/cart/" + cart.id()))
+                    .body(mapper.toModel(cart));
+
+        } catch (Throwable t) {
+            span.recordException(t);
+            throw t;
+        } finally {
+            span.end();
+        }
+
+
+
     }
 
     @DeleteMapping("/{id}")
