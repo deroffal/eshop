ebf83e59e72a7cfa68fd846684c21cf25dfb5ca8
deroffal <32491733+deroffal@users.noreply.github.com>
2024-10-16 20:53:43 +0200

    utility methods

--- applications/eshop-marketplace/src/main/java/fr/deroffal/eshop/marketplace/api/CartController.java
+++ applications/eshop-marketplace/src/main/java/fr/deroffal/eshop/marketplace/api/CartController.java
@@ -2,33 +2,27 @@ package fr.deroffal.eshop.marketplace.api;
 
 import fr.deroffal.eshop.marketplace.domain.model.Cart;
 import fr.deroffal.eshop.marketplace.domain.service.CartService;
-import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.time.Duration;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequestMapping("/cart")
 public class CartController {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(CartController.class);
 
     private final CartService service;
     private final CartMapper mapper;
@@ -42,67 +36,84 @@ public class CartController {
 
     @PostMapping("/")
     public ResponseEntity<CartModel> newCart() {
-        Span span = tracer.spanBuilder("newCart-span")
-                .startSpan();
-
-        try (Scope scope = span.makeCurrent()) {
+        return executeInSpan("newCart-span", span -> {
             Cart cart = service.create();
 
             span.addEvent("cart created", Attributes.of(
                     AttributeKey.stringKey("date"), DateTimeFormatter.ISO_INSTANT.format(Instant.now()),
-                    AttributeKey.stringKey("id"),  cart.id().toString()
+                    AttributeKey.stringKey("id"), cart.id().toString()
             ));
             span.setAttribute("cart", cart.id().toString());
 
 
             return ResponseEntity.created(URI.create("/cart/" + cart.id()))
                     .body(mapper.toModel(cart));
+        });
+    }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void deleteCart(@PathVariable("id") UUID id) {
+        executeInSpan("delete-cart", span -> {
+            span.setAttribute("cart", id.toString());
+            service.delete(id);
+        });
+    }
+
+    @PutMapping("/{id}")
+    public CartModel updateItem(@PathVariable("id") UUID cartId, @RequestBody ItemModel item) {
+        return executeInSpan("cart", span -> {
+            span.setAttribute("cart", cartId.toString());
+            return mapper.toModel(service.updateItem(cartId, mapper.toItem(item)));
+        });
+
+    }
+
+    @GetMapping("/{id}")
+    public CartModel getCart(@PathVariable("id") UUID id) {
+        return executeInSpan("newCart-span", span -> {
+            span.setAttribute("cart", id.toString());
+            return mapper.toModel(service.getByUuid(id));
+        });
+    }
+
+    @GetMapping("/{id}/price")
+    public CartPriceModel getPrice(@PathVariable("id") UUID id) {
+        return executeInSpan("cart-price", span -> {
+            span.setAttribute("cart", id.toString());
+
+            var price = service.getPrice(id);
+            span.addEvent("price calculated", Attributes.of(
+                    AttributeKey.stringKey("id"), id.toString(),
+                    AttributeKey.stringKey("price"), price.toString()
+
+            ));
+
+            return mapper.from(id, price);
+        });
+    }
+
+    private <T> T executeInSpan(String spanName, Function<Span, T> function) {
+        Span span = tracer.spanBuilder(spanName).startSpan();
+        try (Scope ignored = span.makeCurrent()) {
+            return function.apply(span);
         } catch (Throwable t) {
             span.recordException(t);
             throw t;
         } finally {
             span.end();
         }
-
-
-
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(NO_CONTENT)
-    public void deleteCart(@PathVariable("id") UUID id) {
-        Span span = tracer.spanBuilder("cart").startSpan();
-        span.setAttribute("cart", id.toString());
-        service.delete(id);
-    }
-
-    @PutMapping("/{id}")
-    public CartModel updateItem(@PathVariable("id") UUID cartId, @RequestBody ItemModel item) {
-        Span span = tracer.spanBuilder("cart").startSpan();
-        span.setAttribute("cart", cartId.toString());
-        return mapper.toModel(service.updateItem(cartId, mapper.toItem(item)));
-    }
-
-    @GetMapping("/{id}")
-    public CartModel getCart(@PathVariable("id") UUID id) {
-        Span span = tracer.spanBuilder("cart").startSpan();
-        span.setAttribute("cart", id.toString());
-        return mapper.toModel(service.getByUuid(id));
-    }
-
-    @GetMapping("/{id}/price")
-    public CartPriceModel getPrice(@PathVariable("id") UUID id) {
-        Span span = tracer.spanBuilder("cart").startSpan();
-        span.setAttribute("cart", id.toString());
-
-        var price = service.getPrice(id);
-        span.addEvent("price calculated", Attributes.of(
-                AttributeKey.stringKey("id"),  id.toString(),
-                AttributeKey.stringKey("price"),  price.toString()
-
-        ));
-
-        return mapper.from(id, price);
+    private void executeInSpan(String spanName, Consumer<Span> consumer) {
+        Span span = tracer.spanBuilder(spanName).startSpan();
+        try (Scope ignored = span.makeCurrent()) {
+            consumer.accept(span);
+        } catch (Throwable t) {
+            span.recordException(t);
+            throw t;
+        } finally {
+            span.end();
+        }
     }
 }
