80d5f121bed7da396ffc5eb23ba4f42041b96109
deroffal <32491733+deroffal@users.noreply.github.com>
2026-01-23 18:20:01 +0100

    springboot 4

--- applications/eshop-price/src/test/java/fr/deroffal/eshop/price/api/CartEndpointTest.java
+++ applications/eshop-price/src/test/java/fr/deroffal/eshop/price/api/CartEndpointTest.java
@@ -8,7 +8,7 @@ import fr.deroffal.eshop.price.domain.model.Price;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.webflux.test.autoconfigure.WebFluxTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
--- applications/eshop-price/src/test/java/fr/deroffal/eshop/price/api/PriceEndpointTest.java
+++ applications/eshop-price/src/test/java/fr/deroffal/eshop/price/api/PriceEndpointTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.webflux.test.autoconfigure.WebFluxTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
