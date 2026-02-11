8e7653541e6a7e7d0d518f6ab90828d11a254934
deroffal <32491733+deroffal@users.noreply.github.com>
2026-02-11 21:49:23 +0100

    chore: migrate cucumber

--- applications/eshop-price/src/test/java/fr/deroffal/eshop/price/domain/cucumber/DomainCucumberTest.java
+++ applications/eshop-price/src/test/java/fr/deroffal/eshop/price/domain/cucumber/DomainCucumberTest.java
@@ -1,14 +1,17 @@
 package fr.deroffal.eshop.price.domain.cucumber;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        glue = {"fr.deroffal.eshop.price.domain"},
-        features = {"src/test/resources/features/"},
-        plugin = {"json:target/cucumber.json"}
-)
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber.json")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "fr.deroffal.eshop.price.domain")
 public class DomainCucumberTest {
 }
